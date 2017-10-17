202be1e7c1f74b9099ef98d366ea6a9f825db953
Hani Hussein <hani.mohamed.hussein@gmail.com>
2017-10-17 23:35:50 +0200

    Sign in and out and charge balance Activities

--- OTGSTORE/app/src/main/java/com/example/tom/otgstore/MainActivity.java
+++ OTGSTORE/app/src/main/java/com/example/tom/otgstore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tom.otgstore;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -28,82 +29,39 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int RC_SIGN_IN = 111;
 
-    //Firebase instance variables
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Get the views of the MainActivity
-        Button ProfileButton = (Button) findViewById(R.id.buttonProfile);
-        Button BalanceButton = (Button) findViewById(R.id.buttonBalance);
-        Button HistoryButton = (Button) findViewById(R.id.buttonHistory);
-        Button SignOutButton = (Button) findViewById(R.id.buttonSignOut);
-        Button ShoppingButton = (Button) findViewById(R.id.buttonShopping);
-        //set listeners for the buttons
-        ProfileButton.setOnClickListener((View.OnClickListener) this);
-        BalanceButton.setOnClickListener((View.OnClickListener) this);
-        HistoryButton.setOnClickListener((View.OnClickListener) this);
-        SignOutButton.setOnClickListener((View.OnClickListener) this);
-        ShoppingButton.setOnClickListener((View.OnClickListener) this);
 
-        //Initialize Firebase components
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        SharedPreferences prefs = getSharedPreferences("Login", MODE_PRIVATE);
+        Boolean isLogin = prefs.getBoolean("isLogin", false);
+        if (!isLogin ) {
+           Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }else {
 
-        //the listener of the firebase to launch FirebaseUi singing if the user not logged in
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            setContentView(R.layout.activity_main);
 
-            /**
-             * The sign in operation using FirebaseUi
-             * */
-
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Toast.makeText(MainActivity.this, "You're now signed in. Welcome to OTG Store.", Toast.LENGTH_SHORT).show();
-                } else {
-                    //User is signed out
-                    //onSignedOutCleanup();
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(false)
-                                    .setAvailableProviders(
-                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                                    .build(),
-                            RC_SIGN_IN);
-
-                }
+            //Get the views of the MainActivity
+            Button ProfileButton = (Button) findViewById(R.id.buttonProfile);
+            Button BalanceButton = (Button) findViewById(R.id.buttonBalance);
+            Button HistoryButton = (Button) findViewById(R.id.buttonHistory);
+            Button SignOutButton = (Button) findViewById(R.id.buttonSignOut);
+            Button ShoppingButton = (Button) findViewById(R.id.buttonShopping);
+            //set listeners for the buttons
+            ProfileButton.setOnClickListener((View.OnClickListener) this);
+            BalanceButton.setOnClickListener((View.OnClickListener) this);
+            HistoryButton.setOnClickListener((View.OnClickListener) this);
+            SignOutButton.setOnClickListener((View.OnClickListener) this);
+            ShoppingButton.setOnClickListener((View.OnClickListener) this);
 
 
-            }
-        };
-
-    }//End of onCreate
-
-
-
-    //Implement onActivityResult for cancelled FirebaseUI Auth
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) {
-                // Sign-in succeeded, set up the UI
-                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
-            } else if (resultCode == RESULT_CANCELED) {
-                // Sign in was canceled by the user, finish the activity
-                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
-                finish();
-            }
         }
     }
+
+
 
 
     //implement the onClick method here
@@ -128,10 +86,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(shopping);
                 break;
             case R.id.buttonSignOut:
-                //make sign out not implemented yet
-                //there is a problem the activty closed after sign in i'll figure it later
-                FirebaseAuth.getInstance().signOut();
-                finish();
+                SharedPreferences.Editor editor = getSharedPreferences("Login", MODE_PRIVATE).edit();
+                editor.putBoolean("isLogin", false);
+                editor.apply();
+
                 break;
             default:
                 break;
@@ -139,18 +97,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-//This part related to firebase Ui
     @Override
     protected void onResume() {
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
     @Override
     protected void onPause() {
         super.onPause();
-        if (mAuthStateListener != null) {
-            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-        }
+
     }
 
 }
